fa6352c5cc09d3d1c2058914171835a419c0b053
pingzhuang <pingzhuang@hotmail.com>
2020-08-15 14:03:35 +1000

    add more files

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace DemoConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World By Demo!");
         }
     }
 }
